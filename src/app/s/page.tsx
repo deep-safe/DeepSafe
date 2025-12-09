bd7645cd59c9540d9753dcf897ded34521dd790e
simo-hue <mattioli.simone.10@gmail.com>
2025-12-09 12:09:52 +0100

    adult page

--- src/app/s/page.tsx
+++ src/app/s/page.tsx
@@ -177,7 +177,7 @@ export default function SeniorLandingPage() {
                                     <svg fill="currentColor" viewBox="0 0 32 32" aria-hidden="true"><path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z"></path></svg>
                                 </div>
                                 <p className="text-xl md:text-3xl text-gray-800 font-serif italic mb-8 leading-relaxed">
-                                    "Pensavo che internet non facesse per me. Grazie a DeepSafe ho imparato a mandare le foto ai miei nipoti senza paura di sbagliare. Ora mi sento più sicura e autonoma."
+                                    "Pensavo che internet non facesse per me. Grazie a DeepSafe ho imparato a navigare senza paura di sbagliare. Ora mi sento più sicura e autonoma."
                                 </p>
                                 <div>
                                     <p className="font-bold text-gray-900 text-xl">Maria G.</p>
